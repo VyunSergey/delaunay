b116a840df1b33faca12957b68b4401625319728
Vyun Sergey <vyun.sergey@icloud.com>
2022-07-13 12:05:32 +0300

    add import from file improve README

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,4 +1,5 @@
-import scala.util.Try
+import scala.io.Source
+import scala.util.{Try, Using}
 
 object Main {
   def main(args: Array[String]): Unit = {
@@ -8,13 +9,19 @@ object Main {
       Try(args.head.split(";").map(_.split(",").take(2).map(_.toDouble))).toOption
     println(s"Input Points: ${inputPoints.map(_.map(_.mkString("[", ", ", "]")).mkString("[", ", ", "]"))}")
 
+    val filePoints: Option[Array[Array[Double]]] =
+      Using(Source.fromFile(args.head)) { source =>
+        source.getLines.toArray.tail.map(_.split(";").take(2).map(_.toDouble))
+      }.toOption
+    println(s"File Points: ${filePoints.map(_.map(_.mkString("[", ", ", "]")).mkString("[", ", ", "]"))}")
+
     val defaultPoints: Array[Array[Double]] = Array(
       Array(0.0, 0.0), Array(0.0, 3.0), Array(1.0, 2.0), Array(3.0, 1.0), Array(4.0, 4.0),
       Array(7.0, 3.0), Array(6.0, 6.0), Array(2.0, 6.0), Array(9.0, 5.0), Array(10.0, 10.0)
     )
     println(s"Default Points: ${defaultPoints.map(_.mkString("[", ", ", "]")).mkString("[", ", ", "]")}")
 
-    val delaunay = new Delaunay(inputPoints.getOrElse(defaultPoints))
+    val delaunay = new Delaunay(inputPoints.orElse(filePoints).getOrElse(defaultPoints))
 
     val convex = delaunay.getConvexHull
     println(s"Points: ${delaunay.points.map(_.mkString("[", ", ", "]")).mkString("[", ", ", "]")}")
