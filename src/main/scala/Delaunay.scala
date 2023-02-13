fb7877b95cfa3b972735099ab27eefa2cf41a307
Vyun Sergey <vyun.sergey@icloud.com>
2023-02-13 21:29:04 +0300

    refactoring

--- src/main/scala/Delaunay.scala
+++ src/main/scala/Delaunay.scala
@@ -3,6 +3,7 @@ import scala.collection.mutable
 import Delaunay._
 
 class Delaunay(val points: Array[Array[Double]]) {
+  // Граф триангуляцию Делоне в виде пары точек (p, q) -> Set - множество точек вершин треугольника на ребре (p, q)
   val graph: mutable.Map[(Int, Int), Set[Int]] =
     mutable.HashMap.empty[(Int, Int), Set[Int]].withDefaultValue(Set.empty[Int])
 
@@ -18,18 +19,22 @@ class Delaunay(val points: Array[Array[Double]]) {
     graphToConvexHull(graph)
   }
 
-  private val convexPoints: Array[Array[Int]] = Array.fill(points.length)(Array.fill(2)(0))
+  private val convexPoints: Array[Convex] = Array.fill(points.length)(Convex(0, 0))
   private val recursionStack: mutable.Queue[(Int, Int)] = mutable.Queue.empty[(Int, Int)]
 
   private def build(): Unit = {
     if (points.length >= 3) {
       points.sortInPlaceBy(_.head)
 
-      // Инициализация первого треугольника
-      convexPoints(0) = Array(1, 1)
-      convexPoints(1) = Array(0, 0)
-      graph += (0, 1) -> (graph((0, 1)) + 2)
+      // Инициализация первой пары точек
+      // У точки 0 слева и справа на границе следующей точкой является точка 1
+      convexPoints(0) = Convex(left = 1, right = 1)
+      // У точки 1 слева и справа на границе следующей точкой является точка 0
+      convexPoints(1) = Convex(left = 0, right = 0)
+      // У пары точек (0, 1) делаем вершину в точке 2
+      graph += (0, 1) -> Set(2)
 
+      // Начиная с точки 2 строим триангуляцию Делоне
       for (i <- 2 until points.length) AddPointToTriangulation(i)
     }
   }
@@ -39,11 +44,17 @@ class Delaunay(val points: Array[Array[Double]]) {
   // В цикле происходит обход всех точек на границе триангуляции: по часовой и против часовой
   // и для них добавляются новые треугольники с вершинами в точке i
   private def AddPointToTriangulation(i: Int): Unit = {
+    val pointI: Point = Point(points(i))
+
     var prevHullPt: Int = -1
     var hullPt: Int = i - 1
-    var nextHullPt: Int = convexPoints(hullPt)(1)
-    var vector: Array[Double] = vec(points(hullPt), points(i))
-    var nextVector: Array[Double] = vec(points(nextHullPt), points(i))
+    var nextHullPt: Int = convexPoints(hullPt).right
+
+    var pointHull: Point = Point(points(hullPt))
+    var pointHullNext: Point = Point(points(nextHullPt))
+
+    var vector: Vector = vec(pointHull, pointI)
+    var nextVector: Vector = vec(pointHullNext, pointI)
 
     // Двигаясь вправо от точки, пока ребра видимые, выполняем рекурсивное перестроение
     while(crossProduct(vector, nextVector) > -EPS && prevHullPt != nextHullPt) {
@@ -52,17 +63,24 @@ class Delaunay(val points: Array[Array[Double]]) {
 
       prevHullPt = hullPt
       hullPt = nextHullPt
-      nextHullPt = convexPoints(hullPt)(1)
+      nextHullPt = convexPoints(hullPt).right
+
+      pointHullNext = Point(points(nextHullPt))
+
       vector = nextVector
-      nextVector = vec(points(nextHullPt), points(i))
+      nextVector = vec(pointHullNext, pointI)
     }
     val newRight = hullPt
 
     prevHullPt = -1
     hullPt = i - 1
-    nextHullPt = convexPoints(hullPt)(0)
-    vector = vec(points(hullPt), points(i))
-    nextVector = vec(points(nextHullPt), points(i))
+    nextHullPt = convexPoints(hullPt).left
+
+    pointHull = Point(points(hullPt))
+    pointHullNext = Point(points(nextHullPt))
+
+    vector = vec(pointHull, pointI)
+    nextVector = vec(pointHullNext, pointI)
 
     // Аналогично для движения влево, пока ребра видимые
     while(crossProduct(vector, nextVector) < EPS && prevHullPt != nextHullPt) {
@@ -71,18 +89,21 @@ class Delaunay(val points: Array[Array[Double]]) {
 
       prevHullPt = hullPt
       hullPt = nextHullPt
-      nextHullPt = convexPoints(hullPt)(0)
+      nextHullPt = convexPoints(hullPt).left
+
+      pointHullNext = Point(points(nextHullPt))
+
       vector = nextVector
-      nextVector = vec(points(nextHullPt), points(i))
+      nextVector = vec(pointHullNext, pointI)
     }
     val newLeft = hullPt
 
-    convexPoints(i) = Array(newLeft, newRight)
+    convexPoints(i) = Convex(left = newLeft, right = newRight)
 
     // Справа от newLeft теперь i
-    convexPoints(newLeft)(1) = i
+    convexPoints(newLeft).right = i
     // Слева от newRight теперь i
-    convexPoints(newRight)(0) = i
+    convexPoints(newRight).left = i
   }
 
   // Рекурсивная проверка и исправление триангуляции таким образом,
@@ -102,15 +123,15 @@ class Delaunay(val points: Array[Array[Double]]) {
 
       if (CheckDelaunayCondition(left, right, inner, outer)) {
         // Если менять ничего в четырехугольнике не надо, добавляем недостающие ребра
-        graph += (right, outer) -> (getByPair((right, outer)) + left)
-        graph += ( left, outer) -> (getByPair(( left, outer)) + right)
-        graph += ( left, right) -> (getByPair(( left, right)) + outer)
+        graph += (right, outer) -> (graph((right, outer)) + left)
+        graph += ( left, outer) -> (graph(( left, outer)) + right)
+        graph += ( left, right) -> (graph(( left, right)) + outer)
       } else {
         // Иначе перестраиваем триангуляцию в четырехугольнике
-        graph += ( left, outer) -> (getByPair(( left, outer)) - right + inner)
-        graph += (right, outer) -> (getByPair((right, outer)) -  left + inner)
-        graph += orderPair( left, inner) -> (getByPair(orderPair( left, inner)) - right + outer)
-        graph += orderPair(right, inner) -> (getByPair(orderPair(right, inner)) -  left + outer)
+        graph += ( left, outer) -> (graph(( left, outer)) - right + inner)
+        graph += (right, outer) -> (graph((right, outer)) -  left + inner)
+        graph += orderPair( left, inner) -> (graph(orderPair( left, inner)) - right + outer)
+        graph += orderPair(right, inner) -> (graph(orderPair(right, inner)) -  left + outer)
         graph -= ((left, right))
 
         // И добавляем два новых рекурсивных вызова
@@ -128,32 +149,28 @@ class Delaunay(val points: Array[Array[Double]]) {
     if (inner == outer) return true
 
     // Первая точка окружности
-    val p = points(left)
+    val p = Point(points(left))
     // Вторая точка окружности
-    val q = points(right)
+    val q = Point(points(right))
     // Третья точка окружности
-    val r = points(inner)
+    val r = Point(points(inner))
     // Внешняя точка
-    val s = points(outer)
+    val s = Point(points(outer))
 
     // Находим координаты центра окружности по трем точкам
-    val (div, x0, y0) = circle(p, q, r)
+    val Circle(center, div) = circle(p, q, r)
 
     // Проверка на то, три точки не лежат на одной прямой
     if (div == 0) return true
 
     // Находим радиус окружности по трем точкам
-    val pqrR = (div * p(0) - x0) * (div * p(0) - x0) + (div * p(1) - y0) * (div * p(1) - y0)
+    val pqrR = Math.pow(div * p.x - center.x, 2) + Math.pow(div * p.y - center.y, 2)
     // Находим радиус внешней точки
-    val sR = (div * s(0) - x0) * (div * s(0) - x0) + (div * s(1) - y0) * (div * s(1) - y0)
+    val sR =   Math.pow(div * s.x - center.x, 2) + Math.pow(div * s.y - center.y, 2)
 
     // Если радиус внешней точки больше радиуса окружности,
     // то точка лежит за пределами окружности и условие Делоне выполняется
     sR - pqrR > -EPS
-  }
-
-  private def getByPair(pair: (Int, Int)): Set[Int] = {
-    graph(pair)
   }
 
   private def orderPair(left: Int, right: Int): (Int, Int) = {
@@ -165,6 +182,18 @@ object Delaunay {
   private val EPS: Double = 1e-9
 
   def apply(points: Array[Array[Double]] = Array.empty[Array[Double]]) = new Delaunay(points)
+
+  final case class Point(x: Double, y: Double)
+
+  object Point {
+    def apply(arr: Array[Double]): Point = Point(arr(0), arr(1))
+  }
+
+  final case class Convex(var left: Int, var right: Int)
+
+  final case class Vector(x: Double, y: Double)
+
+  final case class Circle(center: Point, div: Double)
 
   // Функция построения триангуляции Делоне множества точек
   def graphToTriangles(graph: mutable.Map[(Int, Int), Set[Int]]): Array[Array[Int]] = {
@@ -187,33 +216,32 @@ object Delaunay {
   }
 
   // Функция вычисления векторного произведения векторов
-  def crossProduct(v1: Array[Double], v2: Array[Double]): Double = {
-    v1(0) * v2(1) - v1(1) * v2(0)
+  def crossProduct(v1: Vector, v2: Vector): Double = {
+    v1.x * v2.y - v1.y * v2.x
   }
 
   // Функция построения вектора по двум точкам
-  def vec(p: Array[Double], q: Array[Double]): Array[Double] = {
-    Array(p(0) - q(0), p(1) - q(1))
+  def vec(p: Point, q: Point): Vector = {
+    Vector(p.x - q.x, p.y - q.y)
   }
 
   // Функция построения описанной окружности вокруг треугольника по трем вершинам
-  def circle(p: Array[Double], q: Array[Double], r: Array[Double]): (Double, Double, Double) = {
-    val pR = p(0) * p(0) + p(1) * p(1)
-    val qR = q(0) * q(0) + q(1) * q(1)
-    val rR = r(0) * r(0) + r(1) * r(1)
+  def circle(p: Point, q: Point, r: Point): Circle = {
+    val pR = p.x * p.x + p.y * p.y
+    val qR = q.x * q.x + q.y * q.y
+    val rR = r.x * r.x + r.y * r.y
 
-    // Нормирующий множитель,
-    // если он равен нулю, значит точки лежат на одной прямой
-    val div = 2 * (p(0) * (q(1) - r(1)) + q(0) * (r(1) - p(1)) + r(0) * (p(1) - q(1)))
+    // Нормирующий множитель, если он равен нулю, значит точки лежат на одной прямой
+    val div = 2 * (p.x * (q.y - r.y) + q.x * (r.y - p.y) + r.x * (p.y - q.y))
     // Координата X0 центра окружности
-    val x0 = -(p(1) * (qR - rR) + q(1) * (rR - pR) + r(1) * (pR - qR))
+    val x = -(p.y * (qR - rR) + q.y * (rR - pR) + r.y * (pR - qR))
     // Координата Y0 центра окружности
-    val y0 =   p(0) * (qR - rR) + q(0) * (rR - pR) + r(0) * (pR - qR)
+    val y =   p.x * (qR - rR) + q.x * (rR - pR) + r.x * (pR - qR)
 
     // Уравнение окружности имеет вид:
-    // (X - X0 / div)^2 + (Y - Y0 / div)^2 = R^2  , где
-    // R^2 = (p(0) - X0 / div)^2 + (p(1) - Y0 / div)^2
+    // (X - X0 / div)^2 + (Y - Y0 / div)^2 = R^2, где
+    // R^2 = (p.x - X0 / div)^2 + (p.y - Y0 / div)^2
 
-    (div, x0, y0)
+    Circle(Point(x, y), div)
   }
 }
--- src/test/scala/CircleSpec.scala
+++ src/test/scala/CircleSpec.scala
@@ -18,11 +18,11 @@ class CircleSpec extends AnyPropSpec with TableDrivenPropertyChecks with should.
                       qx: Double, qy: Double,
                       rx: Double, ry: Double,
                       divExp: Double, x0Exp: Double, y0Exp: Double) =>
-      val (div, x0, y0) = circle(Array(px, py), Array(qx, qy), Array(rx, ry))
+      val Circle(Point(x0, y0), div)    = circle(Point(px, py), Point(qx, qy), Point(rx, ry))
       // println(s"div=$div x0=$x0, y0=$y0")
-      val (div1, x01, y01) = circle(Array(px, py), Array(rx, ry), Array(qx, qy))
-      val (div2, x02, y02) = circle(Array(qx, qy), Array(rx, ry), Array(px, py))
-      val (div3, x03, y03) = circle(Array(rx, ry), Array(px, py), Array(qx, qy))
+      val Circle(Point(x01, y01), div1) = circle(Point(px, py), Point(rx, ry), Point(qx, qy))
+      val Circle(Point(x02, y02), div2) = circle(Point(qx, qy), Point(rx, ry), Point(px, py))
+      val Circle(Point(x03, y03), div3) = circle(Point(rx, ry), Point(px, py), Point(qx, qy))
 
       div should be (divExp)
       x0 should be (x0Exp)
